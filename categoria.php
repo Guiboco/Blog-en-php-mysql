769cb84559aa2d83a37a13d91f912815287ddb39
Guillermo Bonet <guiboco18@gmail.com>
2020-05-08 15:54:09 +0200

    Al pulsar el botón mostrar todas aparecen todas las entradas

---
+++ categoria.php
@@ -0,0 +1,34 @@
+<?php require_once 'includes/cabecera.php' ?>
+<?php require_once 'includes/lateral.php' ?>
+
+
+<!-- CAJA PRINCIPAL -->
+
+<div id="principal">
+    <h1> Ultimas entradas</h1>
+
+    <?php
+    $entradas = conseguirTodasEntradas($db, null);
+    if (!empty($entradas)) :
+        while($entrada = mysqli_fetch_assoc($entradas)): ?>
+            <article class="entradas">
+
+                <a href="">
+                    <h2> <?= $entrada['titulo'] ?></h2>
+                    <span class="fecha"> <?= $entrada['categoria'].' | '. $entrada['fecha']?></span>
+                    <p> <?= substr($entrada['descripcion'], 0, 180)."..." ?> </p>
+                </a>
+            </article>
+    <?php
+        endwhile;
+    endif;
+    ?>
+
+</div>
+
+<div class="clearfix"></div>
+
+
+<!-- fin del contenedor -->
+
+<?php require_once 'includes/pie.php' ?>
